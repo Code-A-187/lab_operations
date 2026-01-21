9a89bb93de372fbcaab361f0533b89bc5ed0bb82
Anton Simeonov <aplamenov@gmail.com>
2026-01-21 13:13:57 +0200

    feat: add registration of user with try/except check if it is unique username and pass, with errors and codes

---
+++ server/services/auth_service.py
@@ -0,0 +1,34 @@
+from core.security import hash_password
+from models.user import User
+from schemas.user import UserCreate
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import IntegrityError
+
+
+def register_new_user(db: Session, user_data: UserCreate):
+    hashed_password = hash_password(user_data.password)
+
+    user_dict = user_data.model_dump(exclude={"password"})
+
+    db_user = User(**user_dict, password_hash = hashed_password)
+    db_user.is_active = False
+
+    # avoids racing conditions when two users try to register email at same time
+    try:
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except IntegrityError as e:
+        db.rollback()
+        error_msg = str(e.orig)
+
+        if "users_email_key" in error_msg:
+            raise HTTPException(status_code = status.HTTP_400_BAD_REQUEST, detail = "Email already registered")
+        raise HTTPException(status_code = status.HTTP_500_INTERNAL_SERVER_ERROR, detail = "Database error")
+
+
+
+
+
