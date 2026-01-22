88389445a5a128c09032c8c68f0dfd364eb59ec6
Anton Simeonov <aplamenov@gmail.com>
2026-01-22 11:51:23 +0200

    fix: register api refactored to async

--- server/api/auth.py
+++ server/api/auth.py
@@ -1,7 +1,7 @@
 
 from fastapi import APIRouter, Depends
 from fastapi import status
-from sqlalchemy.orm import Session
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import get_async_db
 from schemas.user import UserCreate, UserOut
@@ -11,7 +11,6 @@ from services.auth_service import register_new_user
 router = APIRouter(prefix="/auth", tags=["Authentication"])
 
 @router.post("/register", response_model=UserOut, status_code=status.HTTP_201_CREATED)
-def register(user_data: UserCreate, db: Session = Depends(get_async_db)):
-    new_user = register_new_user(db, user_data)
-    
-    return new_user
+async def register(user_data: UserCreate, db: AsyncSession = Depends(get_async_db)):
+    # it gets the data from register form use function to check for duplicates, return the registered user
+    return await  register_new_user(db, user_data)
